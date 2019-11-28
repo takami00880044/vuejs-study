3c0e3df625e13d2c605922eb8d50e90ba68b025d
takami00880044 <takami@key-p.co.jp>
2019-11-28 14:54:20 +0900

    chore: APIアクセストークンを更新

--- src/common/api.js
+++ src/common/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 
 const axiosInstance = axios.create({
   baseURL: 'https://qiita.com/api/v2/',
-  headers: { 'Authorization': 'Bearer 5e1b82069715b6553c4cb799adb5e6b780f2fab7' }
+  headers: { 'Authorization': 'Bearer d6acfe1ba87339677506d1d861a82ce79e8e1f26' }
 })
 
 function getItems () {
